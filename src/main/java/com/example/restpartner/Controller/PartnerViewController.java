8c9fb2777f09c14e63ec7bc2e2378c257ba6b690
tatoon0 <qudwjd4296@naver.com>
2025-07-03 14:13:06 +0900

    commit

--- src/main/java/com/example/restpartner/Controller/PartnerViewController.java
+++ src/main/java/com/example/restpartner/Controller/PartnerViewController.java
@@ -1,4 +1,46 @@
 package com.example.restpartner.Controller;
 
+import com.example.restpartner.DTO.PartnerDTO;
+import com.example.restpartner.Repository.PartnerRepository;
+import com.example.restpartner.Service.PartnerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/partner")
 public class PartnerViewController {
+    private final PartnerService partnerService;
+
+    //목록페이지, /partner/list
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<PartnerDTO> partnerDTOS = partnerService.getAllPartners();
+        model.addAttribute("partnerDTOS", partnerDTOS);
+
+        return "partner/list";
+    }
+
+    //등록페이지, /partner/register
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("partnerDTO", new PartnerDTO());
+
+        return "partner/form";
+    }
+
+    //수정페이지, /partner/update/{id}
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Integer id, Model model) {
+        PartnerDTO partnerDTO = partnerService.getPartnerById(id);
+        model.addAttribute("partnerDTO", partnerDTO);
+
+        return "partner/form";
+    }
 }
