78976df95340b6b15980a5bae4c90a879ebf4fcf
tatoon0 <qudwjd4296@naver.com>
2025-07-03 10:24:54 +0900

    Initial commit

---
+++ src/main/java/com/example/restpartner/RestpartnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.restpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestpartnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestpartnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restpartner/RestpartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restpartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestpartnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
