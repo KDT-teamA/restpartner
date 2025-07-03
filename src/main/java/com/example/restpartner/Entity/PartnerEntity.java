9c5f75a71e9ace2d21afc0b6db2bfd47853842c5
tatoon0 <qudwjd4296@naver.com>
2025-07-03 12:01:00 +0900

    commit

--- src/main/java/com/example/restpartner/Entity/PartnerEntity.java
+++ src/main/java/com/example/restpartner/Entity/PartnerEntity.java
@@ -1,4 +1,36 @@
 package com.example.restpartner.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PartnerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String partnerCode; //거래처코드
+
+    @Column
+    private String partnerName; //거래처명
+
+    @Column
+    private String startDate; //거래시작일
+
+    @Column
+    private Integer quantity; //거래수량
+
+    @Column
+    private Integer unitPrice; //거래가격
+
+    @Column
+    private String manager; //담당자코드
 }
