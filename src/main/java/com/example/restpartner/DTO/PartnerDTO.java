1d7afbc86dc3cce300e9667b1676f4a39f377d55
tatoon0 <qudwjd4296@naver.com>
2025-07-03 12:02:24 +0900

    commit

--- src/main/java/com/example/restpartner/DTO/PartnerDTO.java
+++ src/main/java/com/example/restpartner/DTO/PartnerDTO.java
@@ -1,4 +1,22 @@
 package com.example.restpartner.DTO;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PartnerDTO {
+    private Integer id;
+
+    private String partnerCode; //거래처코드
+
+    private String partnerName; //거래처명
+
+    private String startDate; //거래시작일
+
+    private Integer quantity; //거래수량
+
+    private Integer unitPrice; //거래가격
 }
