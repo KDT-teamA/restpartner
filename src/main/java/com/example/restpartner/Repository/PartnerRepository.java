c735cc72b0d88620cda6d8004fdd1438cc76f786
tatoon0 <qudwjd4296@naver.com>
2025-07-03 12:07:34 +0900

    commit

--- src/main/java/com/example/restpartner/Repository/PartnerRepository.java
+++ src/main/java/com/example/restpartner/Repository/PartnerRepository.java
@@ -1,4 +1,9 @@
 package com.example.restpartner.Repository;
 
-public interface PartnerRepository {
+import com.example.restpartner.Entity.PartnerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartnerRepository extends JpaRepository<PartnerEntity, Integer> {
 }
