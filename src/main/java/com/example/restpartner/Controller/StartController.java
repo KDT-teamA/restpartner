50f01a376e6757b5b298c951cd9a0833f5804e49
tatoon0 <qudwjd4296@naver.com>
2025-07-03 11:39:46 +0900

    commit

--- src/main/java/com/example/restpartner/Controller/StartController.java
+++ src/main/java/com/example/restpartner/Controller/StartController.java
@@ -1,4 +1,12 @@
 package com.example.restpartner.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StartController {
+    @GetMapping
+    public String startController() {
+        return "index";
+    }
 }
